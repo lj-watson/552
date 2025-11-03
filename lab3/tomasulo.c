68c3aad3695e47ac7e2ec85af5a849f2ccf66b0c
Shannon <106455168+purriin@users.noreply.github.com>
2025-11-02 21:02:28 -0500

    Instantiate lab 3 + did some work on fetch and dispatch

---
+++ lab3/tomasulo.c
@@ -0,0 +1,306 @@
+
+#include <limits.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "host.h"
+#include "misc.h"
+#include "machine.h"
+#include "regs.h"
+#include "memory.h"
+#include "loader.h"
+#include "syscall.h"
+#include "dlite.h"
+#include "options.h"
+#include "stats.h"
+#include "sim.h"
+#include "decode.def"
+
+#include "instr.h"
+
+/* PARAMETERS OF THE TOMASULO'S ALGORITHM */
+
+#define INSTR_QUEUE_SIZE         10
+
+#define RESERV_INT_SIZE    4
+#define RESERV_FP_SIZE     2
+#define FU_INT_SIZE        2
+#define FU_FP_SIZE         1
+
+#define FU_INT_LATENCY     4
+#define FU_FP_LATENCY      9
+
+/* IDENTIFYING INSTRUCTIONS */
+
+//unconditional branch, jump or call
+#define IS_UNCOND_CTRL(op) (MD_OP_FLAGS(op) & F_CALL || \
+                         MD_OP_FLAGS(op) & F_UNCOND)
+
+//conditional branch instruction
+#define IS_COND_CTRL(op) (MD_OP_FLAGS(op) & F_COND)
+
+//floating-point computation
+#define IS_FCOMP(op) (MD_OP_FLAGS(op) & F_FCOMP)
+
+//integer computation
+#define IS_ICOMP(op) (MD_OP_FLAGS(op) & F_ICOMP)
+
+//load instruction
+#define IS_LOAD(op)  (MD_OP_FLAGS(op) & F_LOAD)
+
+//store instruction
+#define IS_STORE(op) (MD_OP_FLAGS(op) & F_STORE)
+
+//trap instruction
+#define IS_TRAP(op) (MD_OP_FLAGS(op) & F_TRAP) 
+
+#define USES_INT_FU(op) (IS_ICOMP(op) || IS_LOAD(op) || IS_STORE(op))
+#define USES_FP_FU(op) (IS_FCOMP(op))
+
+#define WRITES_CDB(op) (IS_ICOMP(op) || IS_LOAD(op) || IS_FCOMP(op))
+
+/* FOR DEBUGGING */
+
+//prints info about an instruction
+#define PRINT_INST(out,instr,str,cycle)	\
+  myfprintf(out, "%d: %s", cycle, str);		\
+  md_print_insn(instr->inst, instr->pc, out); \
+  myfprintf(stdout, "(%d)\n",instr->index);
+
+#define PRINT_REG(out,reg,str,instr) \
+  myfprintf(out, "reg#%d %s ", reg, str);	\
+  md_print_insn(instr->inst, instr->pc, out); \
+  myfprintf(stdout, "(%d)\n",instr->index);
+
+/* VARIABLES */
+
+//instruction queue for tomasulo
+static instruction_t* instr_queue[INSTR_QUEUE_SIZE];
+//number of instructions in the instruction queue
+static int instr_queue_size = 0;
+
+//reservation stations (each reservation station entry contains a pointer to an instruction)
+static instruction_t* reservINT[RESERV_INT_SIZE];
+static instruction_t* reservFP[RESERV_FP_SIZE];
+
+//functional units
+static instruction_t* fuINT[FU_INT_SIZE];
+static instruction_t* fuFP[FU_FP_SIZE];
+
+//common data bus
+static instruction_t* commonDataBus = NULL;
+
+//The map table keeps track of which instruction produces the value for each register
+static instruction_t* map_table[MD_TOTAL_REGS];
+
+//the index of the last instruction fetched
+static int fetch_index = 0;
+
+/* FUNCTIONAL UNITS */
+
+
+/* RESERVATION STATIONS */
+
+
+/* 
+ * Description: 
+ * 	Checks if simulation is done by finishing the very last instruction
+ *      Remember that simulation is done only if the entire pipeline is empty
+ * Inputs:
+ * 	sim_insn: the total number of instructions simulated
+ * Returns:
+ * 	True: if simulation is finished
+ */
+static bool is_simulation_done(counter_t sim_insn) {
+  /* ECE552 Assignment 3 - BEGIN CODE */
+
+  // If # of completed instructions = total # instruction, sim is done:
+  return instr_queue_size == sim_insn;
+
+  /* ECE552 Assignment 3 - END CODE */
+}
+
+/* 
+ * Description: 
+ * 	Retires the instruction from writing to the Common Data Bus
+ * Inputs:
+ * 	current_cycle: the cycle we are at
+ * Returns:
+ * 	None
+ */
+void CDB_To_retire(int current_cycle) {
+
+  /* ECE552 Assignment 3 - BEGIN CODE */
+  /* ECE552 Assignment 3 - END CODE */
+
+}
+
+
+/* 
+ * Description: 
+ * 	Moves an instruction from the execution stage to common data bus (if possible)
+ * Inputs:
+ * 	current_cycle: the cycle we are at
+ * Returns:
+ * 	None
+ */
+void execute_To_CDB(int current_cycle) {
+
+  /* ECE552 Assignment 3 - BEGIN CODE */
+  /* ECE552 Assignment 3 - END CODE */
+
+}
+
+/* 
+ * Description: 
+ * 	Moves instruction(s) from the issue to the execute stage (if possible). We prioritize old instructions
+ *      (in program order) over new ones, if they both contend for the same functional unit.
+ *      All RAW dependences need to have been resolved with stalls before an instruction enters execute.
+ * Inputs:
+ * 	current_cycle: the cycle we are at
+ * Returns:
+ * 	None
+ */
+void issue_To_execute(int current_cycle) {
+
+  /* ECE552 Assignment 3 - BEGIN CODE */
+  /* ECE552 Assignment 3 - END CODE */
+}
+
+/* 
+ * Description: 
+ * 	Moves instruction(s) from the dispatch stage to the issue stage
+ * Inputs:
+ * 	current_cycle: the cycle we are at
+ * Returns:
+ * 	None
+ */
+void dispatch_To_issue(int current_cycle) {
+
+  /* ECE552 Assignment 3 - BEGIN CODE */
+
+  /* ECE552 Assignment 3 - END CODE */
+}
+
+/* 
+ * Description: 
+ * 	Grabs an instruction from the instruction trace (if possible)
+ * Inputs:
+ *      trace: instruction trace with all the instructions executed
+ * Returns:
+ * 	None
+ */
+void fetch(instruction_trace_t* trace) {
+
+  /* ECE552 Assignment 3 - BEGIN CODE */
+  // Only fetch if there is room in the queue:
+  if (instr_queue_size < INSTR_QUEUE_SIZE) { 
+    instruction_t* next_instr = NULL;
+
+    // Fetch next instr and skip all TRAP instructions:
+    do {
+      fetch_index++;
+      // If there are no more instructions to fetch, return:
+      if (fetch_index > sim_num_insn) {
+        return;
+      }
+      next_instr = get_instr(trace, fetch_index);
+    } while (next_instr != NULL && IS_TRAP(next_instr->op));
+
+    if (next_instr != NULL) {
+      // Place next instruction at the end of the queue:
+      instr_queue[instr_queue_size] = next_instr;
+      instr_queue_size++;
+    }
+  }
+  /* ECE552 Assignment 3 - END CODE */
+}
+
+/* 
+ * Description: 
+ * 	Calls fetch and dispatches an instruction at the same cycle (if possible)
+ * Inputs:
+ *      trace: instruction trace with all the instructions executed
+ * 	current_cycle: the cycle we are at
+ * Returns:
+ * 	None
+ */
+void fetch_To_dispatch(instruction_trace_t* trace, int current_cycle) {
+
+  fetch(trace);
+
+  /* ECE552 Assignment 3 - BEGIN CODE */
+  // Get the next instruction from the queue:
+  instruction_t* instr = instr_queue[0];
+  if (instr == NULL) {
+    return;
+  }
+
+  instr->tom_dispatch_cycle = current_cycle;
+  /* ECE552 Assignment 3 - END CODE */
+}
+
+/* 
+ * Description: 
+ * 	Performs a cycle-by-cycle simulation of the 4-stage pipeline
+ * Inputs:
+ *      trace: instruction trace with all the instructions executed
+ * Returns:
+ * 	The total number of cycles it takes to execute the instructions.
+ * Extra Notes:
+ * 	sim_num_insn: the number of instructions in the trace
+ */
+counter_t runTomasulo(instruction_trace_t* trace)
+{
+  //initialize instruction queue
+  int i;
+  for (i = 0; i < INSTR_QUEUE_SIZE; i++) {
+    instr_queue[i] = NULL;
+  }
+
+  //initialize reservation stations
+  for (i = 0; i < RESERV_INT_SIZE; i++) {
+      reservINT[i] = NULL;
+  }
+
+  for(i = 0; i < RESERV_FP_SIZE; i++) {
+      reservFP[i] = NULL;
+  }
+
+  //initialize functional units
+  for (i = 0; i < FU_INT_SIZE; i++) {
+    fuINT[i] = NULL;
+  }
+
+  for (i = 0; i < FU_FP_SIZE; i++) {
+    fuFP[i] = NULL;
+  }
+
+  //initialize map_table to no producers
+  int reg;
+  for (reg = 0; reg < MD_TOTAL_REGS; reg++) {
+    map_table[reg] = NULL;
+  }
+  
+  int cycle = 1;
+  while (true) {
+
+    /* ECE552 Assignment 3 - BEGIN CODE */
+    fetch_To_dispatch(trace, cycle);
+    dispatch_To_issue(cycle);
+    issue_To_execute(cycle);
+    execute_To_CDB(cycle);
+    CDB_To_retire(cycle);
+    /* ECE552 Assignment 3 - END CODE */
+
+    cycle++;
+
+    if (is_simulation_done(sim_num_insn))
+      break;
+  }
+  
+  return cycle;
+}
